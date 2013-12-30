bd31f68171e3a0eef200b2be29e100a8be43a7eb
mayone <mayone321@gmail.com>
2013-12-30 14:40:32 +0800

    Create top.v
    module top

---
+++ top.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    27/12/2013 
+// Design Name: 
+// Module Name:    top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: VGA
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module top(CLK, RESET, HSYNC, VSYNC, R, G, B);
+	 
+input   CLK,vRESET;
+output  HSYNC, VSYNC, R, G, B;
+	
+
+
+
+
+
+endmodule
